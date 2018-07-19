92d79b75e580d0083d5e769aae4a4a512dfc46dc
artiship <meilziner@gmail.com>
2018-07-19 23:54:32 +0800

    runnable vs. callable

---
+++ src/test/java/com/me/concurrent/RunnableAndCallable.java
@@ -0,0 +1,16 @@
+package com.me.concurrent;
+
+import org.junit.Test;
+
+public class RunnableAndCallable {
+
+    @Test public void
+    /**
+     *  The Runnable interface is a functional interface and has a single
+     *  run() method which doesn't accept any parameters and does not return
+     *  any values.
+     */
+    runnable() {
+
+    }
+}
